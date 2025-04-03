e7b4d8897d8d5740dcfffceb61782df7ffb104dc
ThomasYang0318 <thomasyang0318@gmail.com>
2025-04-03 13:59:31 +0800

    feat: add scale-space corner detection using image pyramid

--- FAST/main.py
+++ FAST/main.py
@@ -55,24 +55,68 @@ def is_corner(image, p, t, n):
 
     return False
 
+def detect_corners_in_image(image, t, n):
+    keypoints = []
+
+    # 為了避免邊界問題，留 3 個像素的邊界
+    for y in range(3, image.shape[0] - 3):
+        for x in range(3, image.shape[1] - 3):
+            if is_corner(image, (x, y), t, n):
+                keypoints.append((x, y))
+
+    return keypoints
+
+def build_pyramid(image, num_levels, scale_factor):
+    pyramid = [image]
+    for i in range(1, num_levels):
+        # 高斯模糊避免 aliasing
+        blurred = cv2.GaussianBlur(pyramid[-1], (5, 5), 1)
+        # 縮小影像
+        small = cv2.resize(blurred, (0, 0), fx=scale_factor, fy=scale_factor)
+        # 最小影像尺寸
+        if small.shape[0] < 16 or small.shape[1] < 16:
+            break
+        pyramid.append(small)
+    return pyramid
+
+def detect_corners_in_pyramid(pyramid, t, n, scale_factor):
+    all_keypoints = []
+
+    # 針對每一層做 is_corner() 檢測
+    for level , image in enumerate(pyramid):
+        # 計算當前層的 scale
+        scale = scale_factor ** level
+
+        # 找出該層的角點
+        keypoints = detect_corners_in_image(image, t, n)
+
+        # 並把特徵點座標轉回原圖座標
+        for x, y in keypoints:
+            x_original = int(x / scale)
+            y_original = int(y / scale)
+            all_keypoints.append((x_original, y_original))
+
+    return all_keypoints
+
 # 讀取圖片（灰階模式）
 image = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
 
-# 轉成彩色，方便畫紅色圈圈
+# Scale-Space參數設定 
+num_levels = 4      # num_levels: 金字塔層數 
+scale_factor = 0.5  # scale_factor: 縮小倍率
+pyramid = build_pyramid(image, num_levels, scale_factor)
+
+# 角點參數設定 
+t = 20 # t: 亮度差異閾值
+n = 9 # n: 判斷角點條件
+keypoints = detect_corners_in_pyramid(pyramid, t, n, scale_factor)
+
+# 轉成彩色，方便畫紅色圈圈(0, 0, 255)(B, G, R)
 output = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+for x, y in keypoints:
+    cv2.circle(output, (x, y), 1, (0, 0, 255), -1)
 
-# 參數設定
-t = 20  # 亮度差異閾值
-n = 9 # 判斷角點條件
-
-# 遍歷影像中每個像素
-for y in range(3, image.shape[0] - 3):  # 留邊界
-    for x in range(3, image.shape[1] - 3):
-        if is_corner(image, (x, y), t, n):
-            # 畫紅色小圈圈
-            cv2.circle(output, (x, y), 1, (0, 0, 255), -1)
-
-# 顯示結果
-cv2.imshow('Corners', output)
+cv2.imshow('Pyramid Corners', output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
